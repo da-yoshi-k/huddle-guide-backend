899c5c21b084ee4ec372c3ab2e918b5975d3eadd
da-yoshi-k <hey234234s@gmail.com>
2023-03-20 23:26:33 +0900

    add: APIで返却する値にワークショップの作成日時を追加

--- app/resources/workshop_resource.rb
+++ app/resources/workshop_resource.rb
@@ -3,7 +3,7 @@ class WorkshopResource
 
   root_key :workshop, :workshops
 
-  attributes :id, :team_id, :work_id, :work_step_id, :work_date, :facilitator, :presenter, :work_start_time, :turn_start_time
+  attributes :id, :team_id, :work_id, :work_step_id, :work_date, :facilitator, :presenter, :work_start_time, :turn_start_time, :created_at
 
   many :users, resource: UserResource
   one :work, resource: WorkResource
